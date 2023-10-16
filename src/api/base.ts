ea704ea956ee7f343a8f02178684fe983dde6fbd
Roman lesyo <ladiginscormag@gmail.com>
2023-10-16 16:25:30 +0300

    Update base.ts

--- src/api/base.ts
+++ src/api/base.ts
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
-const apiBaseUrl = 'https://chat-demo-4fz7.onrender.com/'
+// const apiBaseUrl = 'https://chat-demo-4fz7.onrender.com/'
 // const apiBaseUrl = 'http://localhost:5500/'
+const apiBaseUrl = 'https://212.86.108.155'
 
 const apiInstance = axios.create({
 	baseURL: apiBaseUrl,
